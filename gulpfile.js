694d264994812f8afab6b9ef1b5a269a296704c6
Faris Karcic <fariskarcic@yahoo.com>
2017-12-06 13:44:28 +0100

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify'),
+concat = require('gulp-concat');
+var connect = require('gulp-connect');
+
+gulp.task('log', function() {
+    gutil.log('== My Log Task ==')
+  });
+
+  gulp.task('js', function() {
+    gulp.src('controllers/*.js')
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('assets'))
+  });
+
+  gulp.task('connect', function() {
+    connect.server({
+      root: '.',
+      livereload: true
+    })
+  });
